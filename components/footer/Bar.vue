f820694bed7dc3185801cf3d665eefe7f4a346cf
JakeDangIt <diam0ndjake07@gmail.com>
2024-08-19 00:33:15 -0400

    mobile screens

--- components/footer/Bar.vue
+++ components/footer/Bar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="bg-theme-red bg-opacity-20 flex justify-between">
-        <div class="flex items-center">
+    <div class="bg-theme-red bg-opacity-20 p-[2px] flex items-center justify-between lg:justify-center">
+        <div class="w-4/5 lg:w-auto flex items-center">
             <Icon name="gravity-ui:circle-letter-c"></Icon>
-            <p class="text-[14px] leading-tight">2024 SITHS Math-a-Thon, all rights reserved.</p>
+            <p class="text-[12px] lg:text-[14px] leading-tight">2024 SITHS Math-a-Thon, all rights reserved.</p>
         </div>
-        <FooterGithubLogoLink />
+        <FooterGithubLogoLink class="ml-2" />
     </div>
 </template>
--- components/header/nav/Menu.vue
+++ components/header/nav/Menu.vue
@@ -5,10 +5,9 @@ const user = useSupabaseUser()
 <template>
   <Sheet>
     <!-- header w/ hamburger icon -->
-    <header class="flex items-center justify-between bg-theme-blue h-12 px-2 py-1">
-
+    <header class="flex items-center justify-between bg-theme-blue h-12">
       <!-- left -->
-      <div class="flex items-center h-full">
+      <div class="flex items-center h-full mx-2">
         <SheetTrigger>
           <!-- hamburger takes time to load but should be fine on initial load -->
           <HeaderHamburgerIcon />
@@ -20,7 +19,6 @@ const user = useSupabaseUser()
         </div>
 
         <h1 class="text-lg">SITHS Math-a-Thon</h1>
-
       </div>
 
       <!-- right -->
@@ -30,42 +28,47 @@ const user = useSupabaseUser()
     </header>
 
     <!-- sheet nav content -->
-    <SheetContent side="left" class="flex h-screen pb-24">
+    <SheetContent side="left" class="h-screen flex flex-col">
       <SheetHeader>
-        <SheetTitle>
+        <SheetTitle class="w-full">
           <div class="rounded-md w-2/6">
             <img src="/math-a-thon-icon.webp">
           </div>
         </SheetTitle>
-        <SheetDescription class="flex flex-col justify-between h-full">
-          <!-- nav links -->
-          <nav class="flex flex-col items-start">
-            <SheetClose v-for="route in routes" class="w-full hover:bg-gray-500 hover:bg-opacity-20">
-              <HeaderNavLink :route-path="route.routePath" :route-name="route.routeName" :icon-name="route.iconName"
-                :variant="'link'" class="text-lg" />
-            </SheetClose>
-          </nav>
-
-          <div class="flex flex-col items-center justify-center">
-            <div v-if="user?.role !== 'authenticated'" class="flex gap-4">
-              <SheetClose>
-                <HeaderNavButtonSignUp />
-              </SheetClose>
-              <SheetClose>
-                <HeaderNavButtonLogIn />
-              </SheetClose>
-            </div>
-            <div v-if="user?.role == 'authenticated'" class="flex gap-4">
-              <SheetClose>
-                <HeaderAvatar />
-              </SheetClose>
-              <SheetClose>
-                <HeaderNavButtonLogOut />
-              </SheetClose>
-            </div>
-          </div>
-        </SheetDescription>
       </SheetHeader>
+
+      <SheetDescription class="flex-1">
+        <!-- nav links -->
+        <nav class="flex flex-col items-start">
+          <SheetClose v-for="route in routes" class="w-full hover:bg-gray-500 hover:bg-opacity-20">
+            <HeaderNavLink :route-path="route.routePath" :route-name="route.routeName" :icon-name="route.iconName"
+              :variant="'link'" class="text-lg" />
+          </SheetClose>
+        </nav>
+      </SheetDescription>
+
+      <SheetFooter class="mt-auto sm:flex-col flex-col gap-4">
+        <div class="flex flex-col items-center justify-center">
+          <div v-if="user?.role !== 'authenticated'" class="flex gap-4">
+            <SheetClose>
+              <HeaderNavButtonSignUp />
+            </SheetClose>
+            <SheetClose>
+              <HeaderNavButtonLogIn />
+            </SheetClose>
+          </div>
+          <div v-if="user?.role == 'authenticated'" class="flex gap-4">
+            <SheetClose>
+              <HeaderAvatar />
+            </SheetClose>
+            <SheetClose>
+              <HeaderNavButtonLogOut />
+            </SheetClose>
+          </div>
+        </div>
+        <FooterBar />
+      </SheetFooter>
     </SheetContent>
   </Sheet>
 </template>
+

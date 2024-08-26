c88e52ee951f50c3b88c4f5ebbf4eebdab58e029
JakeDangIt <diam0ndjake07@gmail.com>
2024-08-26 16:35:01 -0400

    question store and re typeset
    i didnt know nexttick could do that

--- components/questions/Tabs.vue
+++ components/questions/Tabs.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <div v-if="isLoading" class="mx-2 space-y-2">
+        <div v-if="questionsStore.isLoading" class="mx-2 space-y-2">
             <Skeleton class="h-10 w-full mb-4" />
             <Skeleton class="h-24 w-full" />
             <Skeleton class="h-24 w-full" />
         </div>
-        <Tabs :default-value="1" class="mx-auto lg:w-2/3" :class="isLoading ? 'opacity-0' : ''">
+        <Tabs :default-value="1" class="mx-auto lg:w-2/3" :class="questionsStore.isLoading ? 'opacity-0' : ''"
+            @update:model-value="onTabChange">
             <Carousel class="relative w-3/5 mx-auto">
                 <CarouselContent>
                     <CarouselItem v-for="(weeks, index) in filteredWeekData" :key="index">
@@ -31,54 +32,16 @@
 </template>
 
 <script setup>
-import { toast } from '../ui/toast';
-
-const supabase = useSupabaseClient()
-const toastStore = useToastStore()
-
-const questionData = ref([])
-
-const isLoading = ref(true)
-
-async function getQuestions() {
-    const { data, error } = await supabase
-        .from('questions')
-        .select('*')
-
-    if (error) {
-        toastStore.changeToast('Error retrieving questions', error.message)
-        return
-    }
-    questionData.value = data
-}
-
-function getMathJax() {
-    const script = document.createElement('script')
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
-    script.async = true
-    document.head.appendChild(script)
-    script.onload = () => {
-        isLoading.value = false
-    }
-    script.onerror = () => {
-        toastStore.changeToast('Error loading MathJax', 'Please refresh the page')
-        isLoading.value = false
-    }
-}
-
-onMounted(async () => {
-    await getQuestions()
-    getMathJax()
-})
+const questionsStore = useQuestionsStore()
 
 const weekData = [[[1, false], [1, true]], [[2, false], [2, true]], [[3, false], [3, true]]]
 
-const week1Data = computed(() => questionData.value.filter(question => question.week == 1 && question.bonus == false))
-const week1BonusData = computed(() => questionData.value.filter(question => question.week == 1 && question.bonus == true))
-const week2Data = computed(() => questionData.value.filter(question => question.week == 2 && question.bonus == false))
-const week2BonusData = computed(() => questionData.value.filter(question => question.week == 2 && question.bonus == true))
-const week3Data = computed(() => questionData.value.filter(question => question.week == 3 && question.bonus == false))
-const week3BonusData = computed(() => questionData.value.filter(question => question.week == 3 && question.bonus == true))
+const week1Data = computed(() => questionsStore.questionData.filter(question => question.week == 1 && question.bonus == false))
+const week1BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 1 && question.bonus == true))
+const week2Data = computed(() => questionsStore.questionData.filter(question => question.week == 2 && question.bonus == false))
+const week2BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 2 && question.bonus == true))
+const week3Data = computed(() => questionsStore.questionData.filter(question => question.week == 3 && question.bonus == false))
+const week3BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 3 && question.bonus == true))
 
 const weekNames = [1, '1Bonus', 2, '2Bonus', 3, '3Bonus']
 const weekDataValues = [
@@ -97,4 +60,18 @@ const filteredWeekData = computed(() => {
         return weekDataValues[weekIndex].value.length > 0 || weekDataValues[bonusIndex].value.length > 0;
     });
 });
+
+function onTabChange() {
+    nextTick(() => {
+        questionsStore.rerenderMathJax();
+    });
+}
+
+onMounted(() => {
+    if (questionsStore.questionData.length !== 0) {
+        nextTick(() => {
+            questionsStore.rerenderMathJax();
+        });
+    }
+})
 </script>

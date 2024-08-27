d448d78da907ed862ec6c58b5ef3c3f66d4dc65d
JakeDangIt <diam0ndjake07@gmail.com>
2024-08-26 21:49:44 -0400

    footer spacing and break in text if mobile

--- components/footer/Bar.vue
+++ components/footer/Bar.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="bg-theme-red bg-opacity-20 p-[2px] flex items-center justify-between lg:justify-center">
         <div class="w-4/5 lg:w-auto flex items-center">
-            <Icon name="gravity-ui:circle-letter-c"></Icon>
-            <p class="text-[12px] lg:text-[14px] leading-tight">2024 SITHS Math-a-Thon, all rights reserved.</p>
+            <Icon name="gravity-ui:circle-letter-c" class="mx-1"></Icon>
+            <p class="text-[12px] lg:text-[14px] leading-tight">2024 SITHS Math-a-Thon,<br v-if="width < 1024"> all rights reserved.</p>
         </div>
         <FooterGithubLogoLink class="ml-2" />
     </div>
 </template>
+
+<script setup>
+const { width, height } = useWindowSize();
+</script>

d32de7a089ae609657c90315f37a3baea2f90e62
JakeDangIt <diam0ndjake07@gmail.com>
2024-08-28 20:11:17 -0400

    quickfix
    reduced unnecessary filtering and code
    fixed buttons not corresponding to the preview carousel

--- components/questions/Tabs.vue
+++ components/questions/Tabs.vue
@@ -10,13 +10,13 @@
             @update:model-value="onTabChange">
             <Carousel class="relative w-3/5 mx-auto">
                 <CarouselContent>
-                    <CarouselItem v-for="(weeks, index) in filteredWeekData" :key="index">
+                    <CarouselItem v-for="(weekPair) in presentWeekNames" :key="weekPair">
                         <TabsList class="grid w-full grid-cols-2">
-                            <TabsTrigger :value="weeks[0][0]">
-                                Week {{ weeks[0][0] }}
+                            <TabsTrigger :value="weekPair[0]">
+                                Week {{ weekPair[0] }}
                             </TabsTrigger>
-                            <TabsTrigger :value="weeks[1][0] + ' Bonus'">
-                                Week {{ weeks[1][0] }} Bonus
+                            <TabsTrigger :value="weekPair[1]">
+                                Week {{ weekPair[1] }}
                             </TabsTrigger>
                         </TabsList>
                     </CarouselItem>
@@ -26,8 +26,10 @@
             </Carousel>
             <TabsContent v-for="(_, index) in weekNames" :value="weekNames[index]" class="mx-2 space-y-2">
                 <h1 class="text-xl text-center font-bold">Week {{ weekNames[index] }} Questions</h1>
-                <QuestionsQuestionCard v-for="question in weekDataValues[index].value" :key="question.question"
-                    :questionNumber="question.question" :mathContent="question.tex_content" :week="weekNames[index]" />
+                <QuestionsQuestionCard
+                    v-for="question in questionsStore.questionData.filter((question) => question.week == weekNames[index])"
+                    :key="question.question" :questionNumber="question.question" :mathContent="question.tex_content"
+                    :week="weekNames[index]" />
                 <Sheet>
                     <div>
                         <div class="flex items-center gap-2 fixed bottom-3 right-[0.9rem] lg:left-4 lg:right-auto transition-all"
@@ -46,17 +48,17 @@
                                 Ensure your answers are correct before submitting
                             </SheetDescription>
                         </SheetHeader>
-                        <Tabs :default-value="weekNames[index]" class="mx-auto my-2">
-                            <TabsList>
-                                <Carousel class="relative w-3/5 mx-auto">
+                        <Tabs :default-value="weekNames[index]" class="mx-auto my-4">
+                            <TabsList class="w-full">
+                                <Carousel class="relative w-4/5 mx-auto">
                                     <CarouselContent>
-                                        <CarouselItem v-for="(weeks, index) in filteredWeekData" :key="index">
+                                        <CarouselItem v-for="(weekPair, index) in presentWeekNames" :key="index">
                                             <TabsList class="grid w-full grid-cols-2">
-                                                <TabsTrigger :value="weeks[0][0]">
-                                                    <p>Week {{ weeks[0][0] }}</p>
+                                                <TabsTrigger :value="weekPair[0]">
+                                                    <p>Week {{ weekPair[0] }}</p>
                                                 </TabsTrigger>
-                                                <TabsTrigger :value="weeks[1][0] + ' Bonus'">
-                                                    <p class="tracking-tighter">Week {{ weeks[1][0] }} Bonus</p>
+                                                <TabsTrigger :value="weekPair[1]">
+                                                    <p class="tracking-tighter">Week {{ weekPair[1] }}</p>
                                                 </TabsTrigger>
                                             </TabsList>
                                         </CarouselItem>
@@ -71,17 +73,17 @@
                                     v-for="answer in answersStore.answerData.filter(answer => answer.week == weekNames[index]).sort((a, b) => a.questionNumber - b.questionNumber)">
                                     {{ answer.questionNumber }}. {{ answer.answer }}
                                 </p>
+                                <div class="space-x-2 mt-12">
+                                    <Button
+                                        @click="submitAnswers(weekNames[index], answersStore.answerData.filter(answer => answer.week == weekNames[index]))"
+                                        :disabled="submitLoading">
+                                        Submit Week
+                                        {{ weekNames[index] }}
+                                    </Button>
+                                    <Button @click="saveAnswers()" variant="secondary" :disabled="saveLoading">Save
+                                        Answers</Button>
+                                </div>
                             </TabsContent>
-                            <div class="space-x-2 mt-12">
-                                <Button
-                                    @click="submitAnswers(weekNames[index], answersStore.answerData.filter(answer => answer.week == weekNames[index]))"
-                                    :disabled="submitLoading">
-                                    Submit Week
-                                    {{ weekNames[index] }}
-                                </Button>
-                                <Button @click="saveAnswers()" variant="secondary" :disabled="saveLoading">Save
-                                    Answers</Button>
-                            </div>
                         </Tabs>
                         <SheetFooter>
                         </SheetFooter>
@@ -106,32 +108,14 @@ const isFarDownEnough = ref(false)
 const saveLoading = ref(false)
 const submitLoading = ref(false)
 
-const weekData = [[[1, false], [1, true]], [[2, false], [2, true]], [[3, false], [3, true]]]
-
-const week1Data = computed(() => questionsStore.questionData.filter(question => question.week == 1 && question.bonus == false))
-const week1BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 1 && question.bonus == true))
-const week2Data = computed(() => questionsStore.questionData.filter(question => question.week == 2 && question.bonus == false))
-const week2BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 2 && question.bonus == true))
-const week3Data = computed(() => questionsStore.questionData.filter(question => question.week == 3 && question.bonus == false))
-const week3BonusData = computed(() => questionsStore.questionData.filter(question => question.week == 3 && question.bonus == true))
-
 const weekNames = [1, '1 Bonus', 2, '2 Bonus', 3, '3 Bonus']
-const weekDataValues = [
-    week1Data,
-    week1BonusData,
-    week2Data,
-    week2BonusData,
-    week3Data,
-    week3BonusData
-]
+const groupedWeekNames = [[1, '1 Bonus'], [2, '2 Bonus'], [3, '3 Bonus']]
 
-const filteredWeekData = computed(() => {
-    return weekData.filter(weeks => {
-        const weekIndex = weekNames.indexOf(weeks[0][0]);
-        const bonusIndex = weekNames.indexOf(weeks[1][0] + ' Bonus');
-        return weekDataValues[weekIndex].value.length > 0 || weekDataValues[bonusIndex].value.length > 0;
-    });
-});
+const presentWeekNames = computed(() => {
+    return groupedWeekNames.filter((pair) => {
+        return questionsStore.questionData.some((question) => question.week == pair[0])
+    })
+})
 
 function onTabChange() {
     nextTick(() => {

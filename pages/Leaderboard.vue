26bd0477414243515361e16761f50026265753ec
JakeDangIt <diam0ndjake07@gmail.com>
2024-09-06 00:40:28 -0400

    needs comments but i have stats
    in a carousel again, w table for the questions, handles if no submissions, reactive when user answers are loading
    
    needs charts and commetns

--- pages/Leaderboard.vue
+++ pages/Leaderboard.vue
@@ -2,10 +2,13 @@
     <div>
         <div v-if="leaderboardStore.isLoading || leaderboardStore.avatarLoading" class="w-full flex justify-evenly">
             <Skeleton class="mx-2 h-screen w-full lg:w-1/2"></Skeleton>
+            <Skeleton v-if="user || leaderboardStore.userAnswers.length == 0" class="mx-2 h-screen w-full lg:w-1/3">
+            </Skeleton>
         </div>
 
-        <div v-if="!leaderboardStore.isLoading && !leaderboardStore.avatarLoading" class="flex justify-evenly">
-            <LeaderboardTop10 />
+        <div v-if="!leaderboardStore.isLoading && !leaderboardStore.avatarLoading"
+            class="flex flex-col lg:flex-row justify-evenly">
+            <LeaderboardTop10 v-if="(user && leaderboardStore.userAnswers.length > 0) || (!user)" />
             <LeaderboardStats v-if="user" />
         </div>
     </div>

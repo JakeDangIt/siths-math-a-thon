a697d7a54ac2d9d188ccd826e0ac3d75562aef64
JakeDangIt <diam0ndjake07@gmail.com>
2024-09-08 23:49:03 -0400

    directions start
    ill do the rest in class

---
+++ components/home/Directions.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Card class="col-span-full">
+          <CardHeader>
+            <CardTitle>How to Play</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ol class="list-decimal pl-5">
+              <li>Choose a math category</li>
+              <li>Answer questions within the time limit</li>
+              <li>Earn points for correct answers</li>
+              <li>Compete with others on the leaderboard</li>
+            </ol>
+          </CardContent>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,15 @@
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <div class="mx-2 flex flex-col lg:flex-row justify-evenly space-y-2">
-            <HomeBanner class="lg:w-3/5" />
-            <HomeWeekStats class="lg:w-1/5" />
+    <div class="flex flex-col items-center w-full">
+        <div class="lg:w-3/5">
+            <HomeBanner />
+            <!-- <HomeWeekStats class="lg:w-1/5" /> -->
+            <div class="w-full grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                <HomeDirections class="col-span-3" />
+                <HomeDirections />
+                <HomeDirections />
+            </div>
         </div>
     </div>
 </template>

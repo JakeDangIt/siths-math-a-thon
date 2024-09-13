a7a2a97b53a52bac7e183416e2a8d1b59f662ada
Jake Deng <36334856+JakeDangIt@users.noreply.github.com>
2024-09-13 10:28:47 -0400

    takes 2 rows if logged in

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
             <HomeBanner />
             <div class="w-full grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 <HomeDirections class="col-span-full" />
-                <HomeWeekStatsLeaderboard class="md:col-span-2 lg:col-span-1 lg:row-span-2 w-full h-full" />
+                <HomeWeekStatsLeaderboard class="md:col-span-2 lg:col-span-1 w-full h-full" :class="{'lg:row-span-2': user}" />
                 <HomeActivity />
                 <HomeFeaturedProblem />
                 <HomeProfileSummary class="md:col-span-2 lg:col-span-2"/>
@@ -12,3 +12,7 @@
         </div>
     </div>
 </template>
+
+<script setup>
+	const user = useSupabaseUser()
+</setup>

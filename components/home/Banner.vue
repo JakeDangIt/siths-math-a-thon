3a133962d2a0ef18d4dde4564f9a9b769bd483af
Jake Deng <jaked19@nycstudents.net>
2024-09-13 11:41:50 -0400

    file button fix, banner fix

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="">
-        <!-- <picture>
-            <source media="(max-width: 640px)" srcset="/public/math-a-thon-icon.jpg">
-            <img src="/public/banner-placeholder.png">
-        </picture> -->
-        <div className="rounded-lg bg-gradient-to-r from-theme-red to-theme-blue p-6 text-white border-2 border-black">
-            <h1 className="text-3xl font-bold">SITHS Math-a-Thon</h1>
-            <p className="mt-2">-insert some tagline here-</p>
+    <div>
+        <div class="rounded-lg bg-gradient-to-r from-theme-red to-theme-blue p-6 text-white border-2 border-black">
+            <h1 class="text-3xl font-bold">SITHS Math-a-Thon</h1>
+            <p class="mt-2">-insert some tagline here-</p>
         </div>
     </div>
 </template>

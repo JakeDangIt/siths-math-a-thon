3b06df48067a993731a386cc9813e57c5f14b640
Jake Deng <36334856+JakeDangIt@users.noreply.github.com>
2024-10-07 09:34:03 -0400

    media queriesing

--- components/header/nav/Menu.vue
+++ components/header/nav/Menu.vue
@@ -36,6 +36,10 @@ const currentRouteName = computed(() =>
         <HeaderNavButtonSignUp />
         <HeaderNavButtonLogIn class="md:block sm:hidden" />
       </div>
+			<div v-else class="flex justify-end">
+        <HeaderAvatar class="md:block sm:hidden" />
+				<HeaderNavButtonLogOut class="md:block sm:hidden" />
+      </div>
     </header>
 
     <!-- sheet nav content -->

5ae073db774b4af430814da2c4067e336fc45224
Jake Deng <36334856+JakeDangIt@users.noreply.github.com>
2024-10-25 06:21:10 -0700

    add activity and all activity
    fix content shift if user not logged in, no stats so stats and leaderboard should be same size as other cards
    
    finish add activity dialog

--- components/home/Activity.vue
+++ components/home/Activity.vue
@@ -1,24 +1,42 @@
 <template>
   <div>
     <!-- recent activity but idk how i wanna actually put the activity -->
-    <Card class="h-full">
+    <Card class="h-full flex flex-col">
       <CardHeader>
         <CardTitle>Recent Activity</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div v-for="activity in activityStore.activityData" className="space-y-2">
-          <p>{{formatDate(activity.date)}}</p>
-          <p>{{activity.content}}</p>
+      <CardContent class="flex-1">
+        <div v-for="activity in activityStore.activityData.slice(0, 3)" className="space-y-2">
+          <p><span class="font-bold">{{ formatDate(activity.date) }}</span> - {{ activity.content }}</p>
         </div>
       </CardContent>
+      <CardFooter>
+        <Dialog>
+          <DialogTrigger>
+            <Button>View All</Button>
+          </DialogTrigger>
+          <DialogScrollContent>
+            <DialogHeader>
+              <DialogTitle>All Activity</DialogTitle>
+            </DialogHeader>
+            <div>
+              <div v-for="activity in activityStore.activityData" className="space-y-2">
+                <p><span class="font-bold">{{ formatDate(activity.date) }}</span> - {{ activity.content }}</p>
+              </div>
+            </div>
+          </DialogScrollContent>
+        </Dialog>
+        <HomeAddActivity v-if="roleStore.role == 'admin'" />
+      </CardFooter>
     </Card>
   </div>
 </template>
 
 <script setup>
+const roleStore = useRoleStore();
 const activityStore = useActivityStore();
 
 function formatDate(date) {
-  return new Date(date).toLocaleString().split(',')[0].replaceAll('/', '-')
+  return new Date(date).toLocaleString().split(',')[0]
 }
 </script>

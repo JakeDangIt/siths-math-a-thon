9aec553831ea563c4484ae02b31deb74b017c4aa
Jake Deng <36334856+JakeDangIt@users.noreply.github.com>
2024-12-04 06:17:46 -0800

    no iamge doenst break

--- server/api/questions.js
+++ server/api/questions.js
@@ -19,10 +19,8 @@ export default defineEventHandler(async (event) => {
     const QUESTIONS_QUERY = `*[_type == "questions" && week == '${questionInfo.week}' && number == '${questionInfo.number}'][0]`;
     const existingQuestion = await sanityClient.fetch(QUESTIONS_QUERY);
 
-    const base64String = changes.image.split(',')[1];
-    const image = Buffer.from(base64String, 'base64');
     const filePath = `week${questionInfo.week}_question${questionInfo.number}.png`;
-    
+
     if (existingQuestion) {
       await sanityClient
         .patch(existingQuestion._id)
@@ -33,7 +31,10 @@ export default defineEventHandler(async (event) => {
         })
         .commit();
 
-      if (image) {
+      if (changes.image) {
+        const base64String = changes.image.split(',')[1];
+        const image = Buffer.from(base64String, 'base64');
+        
         await sanityClient.assets
           .upload('image', await streamifier.createReadStream(image), {
             filename: filePath,

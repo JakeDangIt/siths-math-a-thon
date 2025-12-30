cfa5f9a6704e02b855a7eee8a4e936813a25c8eb
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-12-30 01:11:18 -0500

    boom thats it

--- components/footer/Bar.vue
+++ components/footer/Bar.vue
@@ -2,10 +2,10 @@
   <div
     class="footer relative flex flex-col items-center justify-center bg-theme-red bg-opacity-20 p-[2px]"
   >
-    <div class="relative z-20 flex items-center text-center py-1">
+    <div class="relative z-20 flex items-center py-1 text-center">
       <Icon name="material-symbols:copyright-outline" class="mx-1" />
       <p class="text-[10px] leading-tight lg:text-[14px]">
-        2025 SITHS Math-a-Thon, all rights reserved.
+        2026 SITHS Math-a-Thon, all rights reserved.
       </p>
       <FooterGithubLogoLink class="ml-1" />
     </div>
@@ -16,7 +16,6 @@
       class="footer-image pointer-events-none absolute bottom-0 left-0 z-0 hidden h-auto w-full"
       draggable="false"
     />
-
   </div>
 </template>
 
